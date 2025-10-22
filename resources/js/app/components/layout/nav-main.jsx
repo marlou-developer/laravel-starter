c73605e8ae4b867e5ea7cca38afc2bcf7942e070
Marlou Dev <webdev@empireonegroup.com>
2025-10-22 22:23:32 +0800

    added accordion

--- resources/js/app/components/layout/nav-main.jsx
+++ resources/js/app/components/layout/nav-main.jsx
@@ -1,10 +1,17 @@
 import { Link, usePage } from '@inertiajs/react';
+import { ChevronDown, User } from 'lucide-react';
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from '../collapsible';
 import {
     SidebarGroup,
     SidebarGroupLabel,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    SidebarMenuSub,
 } from '../sidebar';
 
 export function NavMain({ items = [] }) {
@@ -13,6 +20,60 @@ export function NavMain({ items = [] }) {
         <SidebarGroup className="px-2 py-0">
             <SidebarGroupLabel>Platform</SidebarGroupLabel>
             <SidebarMenu>
+                <Collapsible defaultOpen className="group/collapsible">
+                    <SidebarGroup>
+                        <SidebarMenuButton asChild>
+                            <CollapsibleTrigger >
+                                {/* <div className='flex items-center justify-between'>
+                                  
+                                    Components
+                                </div> */}
+                                {/* <User className='h-4'/> */}
+
+                                <SidebarMenuItem  className="-ml-2">
+                                    <SidebarMenuButton
+                                        asChild
+                                        isActive={page.url
+                                            .startsWith
+                                            // typeof item.href === 'string'
+                                            //     ? item.href
+                                            //     : item.href.url,
+                                            ()}
+                                        tooltip={{ children: 'Components' }}
+                                    >
+                                        <Link href={'/'} prefetch>
+                                            <User className="h-4" />
+                                            <span>Components</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+
+                                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                            </CollapsibleTrigger>
+                        </SidebarMenuButton>
+                        <CollapsibleContent>
+                            {/* <SidebarGroupContent /> */}
+                            {items.map((item) => (
+                                <SidebarMenuSub key={item.title}>
+                                    <SidebarMenuButton
+                                        asChild
+                                        isActive={page.url.startsWith(
+                                            typeof item.href === 'string'
+                                                ? item.href
+                                                : item.href.url,
+                                        )}
+                                        tooltip={{ children: item.title }}
+                                    >
+                                        <Link href={item.href} prefetch>
+                                            {item.icon && <item.icon />}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuSub>
+                            ))}
+                        </CollapsibleContent>
+                    </SidebarGroup>
+                </Collapsible>
                 {items.map((item) => (
                     <SidebarMenuItem key={item.title}>
                         <SidebarMenuButton
--- resources/js/app/components/sidebar.jsx
+++ resources/js/app/components/sidebar.jsx
@@ -276,7 +276,7 @@ function SidebarGroup({ className, ...props }) {
             data-slot="sidebar-group"
             data-sidebar="group"
             className={cn(
-                'relative flex w-full min-w-0 flex-col p-2',
+                'relative flex w-full min-w-0 flex-col ',
                 className,
             )}
             {...props}
